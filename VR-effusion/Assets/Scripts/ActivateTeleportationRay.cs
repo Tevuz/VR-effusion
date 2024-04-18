fc905f1be6abf903bcf8edc40f2c4349e33899a9
KajaPlaszko <597723@stud.hvl.no>
2024-04-18 13:59:26 +0200

    Initial commit

---
+++ VR-effusion/Assets/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+public class ActivateTeleportationRay : MonoBehaviour
+{
+
+    public GameObject rightTeleportation;
+    public GameObject leftTeleportation;
+
+    public InputActionProperty rightActivate;
+    public InputActionProperty rightCancel;
+    public InputActionProperty leftActivate;
+    public InputActionProperty leftCancel;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+
+    }
+
+}
---
+++ VR-effusion/Assets/Scripts/LineRendererSettings.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LineRendererSet : MonoBehaviour
+{
+    public GameObject panel;
+    public Image img;
+    public Button btn;
+
+    [SerializeField] LineRenderer lineRenderer;
+
+    Vector3[] points;
+
+    public LayerMask layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = panel.GetComponent<Image>();
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        points = new Vector3[2];
+
+        points[1] = Vector3.zero;
+
+        points[1] = transform.position + new Vector3(0, 0, 20);
+
+        lineRenderer.SetPositions(points);
+        lineRenderer.enabled = true;
+    }
+
+    public bool AlignLineRenderer(LineRenderer renderer)
+    {
+        bool hitBtn = false;
+        Ray ray;
+        ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(ray, out hit, layerMask))
+        {
+
+
+            points[1] = transform.forward + new Vector3(0, 0, hit.distance);
+            btn = hit.collider.gameObject.GetComponent<Button>();
+
+            hitBtn = true;
+
+        }
+        else
+        {
+            points[1] = transform.forward + new Vector3(0, 0, 20);
+        }
+
+        renderer.SetPositions(points);
+        return hitBtn;
+    }
+
+    public TMP_Text feedbackText; // Dra Text UI-element hit i Inspector
+
+    // Denne metoden laster en scene basert på dens navn
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Denne metoden viser en midlertidig tilbakemelding
+    public void ShowFeedback(string message)
+    {
+        StartCoroutine(ShowFeedbackCoroutine(message));
+    }
+
+    private IEnumerator ShowFeedbackCoroutine(string message)
+    {
+        feedbackText.text = message;
+        feedbackText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2); // Vent i 2 sekunder
+        feedbackText.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (AlignLineRenderer(lineRenderer) && Input.GetAxis("Submit") > 0)
+        {
+            btn.onClick.Invoke();
+        }
+    }
+}

1809bddfa6b8c71049eab4ba271b292dd8c8a858
KajaPlaszko <597723@stud.hvl.no>
2024-04-23 20:45:28 +0200

    gravity, snap turn,

--- VR-effusion/Assets/Scripts/NeedleController.cs
+++ VR-effusion/Assets/Scripts/NeedleController.cs
@@ -6,49 +6,59 @@ public class NeedleController : MonoBehaviour
     private bool isColliding = false;
     public InputActionProperty pinchAction;
     public Animator handAnimator;
+    private Rigidbody rb;
 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
         if (isColliding)
         {
-            // Freeze position in X and Z directions
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+
+
+            // Freeze position in Y and X directions, allow movement in Z
+            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
 
             float triggerValue = pinchAction.action.ReadValue<float>();
             handAnimator.SetFloat("Trigger", triggerValue);
 
-            //Trigger is down -> freeze positions/rotations -> extract liquid 
+            // If trigger is down, freeze all positions/rotations and simulate liquid extraction
             if (triggerValue > 0.5f)
             {
                 rb.constraints = RigidbodyConstraints.FreezeAll;
-                Debug.Log("Høyre trigger er presset ned!");
+            } else
+            {
+                rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+
             }
         }
         else
         {
-            Rigidbody rb = GetComponent<Rigidbody>();
             rb.constraints = RigidbodyConstraints.None;
         }
 
 
     }
-
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Body"))
+        // Når nålen treffer målet, ignorer kollisjoner mellom nålen og målet
+        if (collision.collider.CompareTag("Body"))
         {
             isColliding = true;
+            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Body"))
+        // Når nålen forlater målet, ikke lenger ignorer kollisjoner mellom nålen og målet
+        if (collision.collider.CompareTag("Body"))
         {
             isColliding = false;
         }
     }
 }
-
-
